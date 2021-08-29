2ade54fd554593c0cfddabd717f682142d357a63
Hannah Malm <hannahemalm616@gmail.com>
2021-08-28 21:45:55 -0400

    commit tree 3cf2c4660431e447fef358094b12173e63409f76

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
 end
+
+#Things that arent the concern of a single controller and are applicable to the entire application
+#in application shwere users can log in, the application helper will have a method to expose current user
+
+#Want to dynamically change the title of each page based on what the user is looking at
+def title(text)
+    content_for :title, text 
+end 
+
+#use this context_for custom helper
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+    #displaying the date of the last time the post was updated inst an application-wide concern
+    #this is only a post concern which is why it is in the PostsHlper
+    #need to pass post as an argument because the helper needs to know that we are dealing iwth an instance vairalbe
+    def last_updated(post)
+        post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+    end
 end
